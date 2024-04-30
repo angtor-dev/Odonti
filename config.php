925769416c66b997192cf8c95bb39940f3336cee
Angel Torres <angtor.dev@gmail.com>
2024-04-29 22:15:28 -0400

    commit inicial

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+date_default_timezone_set('America/Caracas');
+define("APP_NAME", "Odonti");
+
+// Default user configs (estas constantes pueden sobrescribirse
+// en el archivo user_config.php)
+defined('LOCAL_DIR') or define("LOCAL_DIR", "/Odonti/");
+
+defined('DEVELOPER_MODE') or define("DEVELOPER_MODE", false);
+
+defined('DB_HOST') or define("DB_HOST", "localhost");
+defined('DB_NAME') or define("DB_NAME", "odonti");
+defined('DB_USER') or define("DB_USER", "root");
+defined('DB_PASSWORD') or define("DB_PASSWORD", "");
+
+// Expresiones regulares
+define("REG_NUMERICO", "/^[0-9]+$/");
+define("REG_ALFABETICO", "/^\s*[a-zA-ZáÁéÉíÍóÓúÚüÜñÑ., ]+\s*$/");
+define("REG_ALFANUMERICO", "/^\s*[0-9a-zA-ZáÁéÉíÍóÓúÚüÜñÑ., ]+\s*$/");
+define("REG_CLAVE", "/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/");
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+if (file_exists("user_config.php"))
+    require_once "user_config.php";
+require_once "config.php";
+require_once "utilities.php";
+session_start();
+
+$defaultController = "Index";
+$defaultPath = "Home/";
+
+// Carpeta relativa actual del proyecto
+$relativePath = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], "index.php"));
+// Elimina la ruta del proyecto, los parametro y el ultimo "/"
+$requestUri = substr($_SERVER['REQUEST_URI'], strlen($relativePath));
+$requestUri = substr($requestUri, 0, strpos($requestUri, '?') === false ? strlen($requestUri) : strpos($requestUri, '?'));
+$requestUri = rtrim($requestUri, "/");
+// Divide la ruta en partes
+$uriParts = explode('/', $requestUri);
+
+$controllerName = count($uriParts) > 1 ? $uriParts[count($uriParts) - 1] : $defaultController;
+$controllerPath = empty($requestUri) ? $defaultPath :
+    (substr($requestUri, 0, strpos($requestUri, $controllerName)) ?: $requestUri."/");
+
+if (is_file("Controllers/".$controllerPath.$controllerName."Controller.php")) {
+    require_once "Controllers/".ucfirst($controllerPath).ucfirst($controllerName)."Controller.php";
+    exit();
+} else {
+    // TODO: usar otras variables para mantener historial de ruta visitada
+    $controllerPath .= $controllerName."/";
+    $controllerName = "Index";
+    if (is_file("Controllers/".ucfirst($controllerPath).$controllerName."Controller.php")) {
+        require_once "Controllers/".ucfirst($controllerPath).$controllerName."Controller.php";
+        exit();
+    }
+}
+
+http_response_code(404);
+if (DEVELOPER_MODE) {
+    // TODO: crear vista para imprimir errores de desarrollador
+    die("<font face=consolas><b>[Error]</b> El controlador <b>".$controllerName."</b> en <b>".$controllerPath."</b> no existe</font>");
+}
+// TODO: Crear vista generica para errores
