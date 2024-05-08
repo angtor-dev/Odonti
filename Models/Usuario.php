c023e56239e803f7e4df9586967ac64c9d6fbd33
Angel Torres <atorres@oviortech.com>
2024-05-08 12:39:00 -0400

    Se implementa clase de Usuario con metodos para logearse y buscar por correo

---
+++ Models/Usuario.php
@@ -0,0 +1,53 @@
+<?php
+require_once "Models/Model.php";
+
+class Usuario extends Model
+{
+    public int $id;
+    public string $correo;
+    public string $nombre;
+    public string $apellido;
+    public int $estado;
+    private string $clave;
+
+    public static function login(string $correo, string $clave) : bool
+    {
+        if (empty($correo) || empty($clave)) {
+            return false;
+        }
+
+        $usuario = Usuario::cargarPorCorreo($correo);
+
+        if (is_null($usuario) || !$usuario->validarClave($clave)) {
+            return false;
+        }
+
+        session_start();
+        $_SESSION['usuario'] = $usuario;
+
+        return true;
+    }
+
+    private function validarClave(string $clave) : bool
+    {
+        return password_verify($clave, $this->clave);
+    }
+
+    public static function cargarPorCorreo(string $correo, int $estado = 1) : Usuario | null
+    {
+        $bd = Database::getInstance();
+        $query = "SELECT * FROM usuario WHERE correo = :correo AND estado = :estado";
+        
+        $stmt = $bd->pdo()->prepare($query);
+        $stmt->bindValue("correo", $correo);
+        $stmt->bindValue("estado", $estado);
+
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, "Usuario");
+
+        if ($stmt->rowCount() == 0) {
+            return null;
+        }
+        return $stmt->fetch();
+    }
+}
