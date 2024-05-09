bab2cf4cfd78244c3b2002e0db33299be33e9a42
Angel Torres <atorres@oviortech.com>
2024-05-09 12:34:32 -0400

    Se agrega libreria FontAwesome 6 al proyecto (iconos)

--- Views/_Plantillas/Principal.php
+++ Views/_Plantillas/Principal.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= LOCAL_DIR ?>public/lib/fontawesome/css/all.min.css">
     <title><?= APP_NAME ?></title>
 </head>
 <body>
