14dd43a36d513139a82ffa5f43f3b64a9eca2a1b
Angel Torres <atorres@oviortech.com>
2024-05-10 17:10:34 -0400

    Se incluyen logos de la UPTAEB al proyecto

--- Views/_Plantillas/Principal.php
+++ Views/_Plantillas/Principal.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?= LOCAL_DIR ?>/public/img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/css/utilities.css">
