8b53e538beded0ed79cdfe9b95753d0530bd909f
Angel Torres <atorres@oviortech.com>
2024-05-13 18:06:15 -0400

    Se implementa menu plegables del sidebar y otro poco e vainas

--- Views/_Componentes/MenuLateral.php
+++ Views/_Componentes/MenuLateral.php
@@ -2,15 +2,19 @@
 
 <div id="menu-lateral" class="sidebar">
     <div class="sidebar-header">
-        Odonti
+        <div class="d-flex align-items-center gap-2">
+            <img src="<?= LOCAL_DIR ?>/public/img/logo-white-2.png" alt="">
+            Odonti
+        </div>
         <div class="sidebar-toggle">
-            A
+            <i class="fa-solid fa-bars"></i>
         </div>
     </div>
     <div class="user acordeon" style="cursor: pointer;">
         <div class="acordeon-toggle d-flex align-items-center">
-            <div class="avatar me-2">
-                <!-- Icono aqui -->
+            <div class="avatar me-2 d-flex align-items-center justify-content-center fs-5">
+                <i class="fa-solid fa-user"></i>
+                <!-- Foto aqui -->
             </div>
             <div class="info gap-1">
                     <span style="font-size: 14px;"><?= $usuario->nombre ?></span>
@@ -22,8 +26,32 @@
                 <div class="mt-3">
                     <a href="#" class="link">Mi perfil</a>
                     <a href="#" class="link">Ajustes</a>
-                    <a href="#" class="link">Cerrar sesión</a>
+                    <a href="<?= LOCAL_DIR ?>/loguot" class="link">Cerrar sesión</a>
                 </div>
+            </div>
+        </div>
+    </div>
+    <a href="<?= LOCAL_DIR ?>" class="sidebar-button mx-3 mt-3
+        <?= empty($uriParts[0]) ? "active" : "" ?>">
+        <i class="fa-solid fa-house-chimney"></i>
+        Dashboard
+    </a>
+    <h4>Seguridad</h4>
+    <a href="<?= LOCAL_DIR ?>/Usuarios" class="sidebar-button mx-3">
+        <i class="fa-solid fa-user"></i>
+        Usuarios
+    </a>
+    <h4>Otros</h4>
+    <div class="mx-3 acordeon">
+        <button class="acordeon-toggle sidebar-button">
+            <i class="fa-solid fa-user"></i>
+            Menu 1
+        </button>
+        <div class="acordeon-body">
+            <div class="acordeon-items py-2">
+                <a href="#">Subopcion 1</a>
+                <a href="#">Subopcion 1</a>
+                <a href="#">Subopcion 1</a>
             </div>
         </div>
     </div>
