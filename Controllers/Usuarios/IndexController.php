1492a018d419b01c65805d1068e83a2d3287bbab
Angel Torres <atorres@oviortech.com>
2024-05-29 18:02:19 -0400

    Se implementa funcionalidad para listar usuarios (o cualquier modelo)

--- Controllers/Usuarios/IndexController.php
+++ Controllers/Usuarios/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 necesitaAutenticacion();
 
-// Codigo aqui
+$usuarios = Usuario::listar();
 
 renderView();
--- Models/Model.php
+++ Models/Model.php
@@ -11,6 +11,25 @@ abstract class Model
         $this->db = Database::getInstance();
     }
 
+    /**
+     * Retorna un array de objetos del modelo que lo instacía
+     *
+     * @param int|null $estado Si no se especifica, retorna todas las filas de la tabla.
+     * Si se especifica, retorna las filas donde el estado sea igual al indicado.
+     * @return array<self>
+     **/
+    public static function listar(int $estado = null): array
+    {
+        $bd = Database::getInstance();
+        $table = strtolower(static::class);
+        $query = "SELECT * FROM $table" . (isset($estado) ? " WHERE estado = $estado" : "");
+
+        $stmt = $bd->pdo()->query($query);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, $table);
+
+        return $stmt->fetchAll();
+    }
+
     /** Shorthand para PDO::query() */
     protected function query(string $query): PDOStatement
     {
