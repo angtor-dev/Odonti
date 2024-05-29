d91e36611eac032f3a63b8c98850b644f3fd8ca0
Angel Torres <atorres@oviortech.com>
2024-05-29 18:00:32 -0400

    Se agrega DataTables al proyecto

--- Views/_Plantillas/Principal.php
+++ Views/_Plantillas/Principal.php
@@ -6,6 +6,7 @@
     <link rel="shortcut icon" href="<?= LOCAL_DIR ?>/public/img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/DataTables/datatables.min.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/css/utilities.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/css/main.css">
 
@@ -23,6 +24,7 @@
     </main>
     
     <script src="<?= LOCAL_DIR ?>/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= LOCAL_DIR ?>/public/lib/DataTables/datatables.min.js"></script>
     <script src="<?= LOCAL_DIR ?>/public/js/utilities.js"></script>
 </body>
 </html>
