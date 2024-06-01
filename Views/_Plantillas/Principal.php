c8a37dd773fd40d6fe1208892c16771aeecee7e8
Angel Torres <angtor.dev@gmail.com>
2024-05-31 21:16:01 -0400

    Se agrega toastify.js al proyecto

--- Views/_Plantillas/Principal.php
+++ Views/_Plantillas/Principal.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/DataTables/datatables.min.css">
+    <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/lib/toastify/toastify.min.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/css/utilities.css">
     <link rel="stylesheet" href="<?= LOCAL_DIR ?>/public/css/main.css">
 
@@ -25,6 +26,7 @@
     
     <script src="<?= LOCAL_DIR ?>/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="<?= LOCAL_DIR ?>/public/lib/DataTables/datatables.min.js"></script>
+    <script src="<?= LOCAL_DIR ?>/public/lib/toastify/toastify.min.js"></script>
     <script src="<?= LOCAL_DIR ?>/public/js/utilities.js"></script>
 </body>
 </html>
