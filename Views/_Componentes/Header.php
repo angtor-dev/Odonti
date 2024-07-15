71b782424561040506a35de530f960ae48dc2e58
Angel Torres <angtor.dev@gmail.com>
2024-07-14 23:35:23 -0400

    ui de notificaciones

--- Views/_Componentes/Header.php
+++ Views/_Componentes/Header.php
@@ -18,9 +18,40 @@
             </a>
         </div>
         <div class="col d-flex align-items-center justify-content-end">
-            <button type="button" class="btn btn-header">
-                <i class="fa-solid fa-bell"></i>
-            </button>
+            <div class="dropdown">
+                <button type="button" class="btn btn-header" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-bell"></i>
+                </button>
+                <ul class="dropdown-menu" style="width: 280px;">
+                    <div class="pb-2 px-3 border-bottom text-center">
+                        <span>Tienes 2 Notificaciones</span>
+                    </div>
+                    <li>
+                        <a class="dropdown-item d-flex p-0" href="#">
+                            <div class="m-2 d-flex justify-content-center align-items-center"
+                                style="width: 40px; height: 40px; background-color: var(--bs-primary);
+                                    color: white; border-radius: 50px">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                            <div class="d-flex flex-column gap-2 py-2">
+                                <span>Notificacion de prueba</span>
+                                <small class="light">Hace 5 minutos</small>
+                            </div>
+                        </a>
+                        <a class="dropdown-item d-flex p-0" href="#">
+                            <div class="m-2 d-flex justify-content-center align-items-center"
+                                style="width: 40px; height: 40px; background-color: var(--bs-primary);
+                                    color: white; border-radius: 50px">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                            <div class="d-flex flex-column gap-2 py-2">
+                                <span>Otra notificacion</span>
+                                <small class="light">Hace 2 horas</small>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </header>
