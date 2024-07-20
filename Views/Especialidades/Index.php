9aded9c73d6bbe592c92adc90a7228f37716acf6
Angel Torres <angtor.dev@gmail.com>
2024-07-20 10:59:36 -0400

    fix especialidad

--- Views/Especialidades/Index.php
+++ Views/Especialidades/Index.php
@@ -7,7 +7,7 @@
                 <h3 class="pb-2">Especialidades</h3>
                 <span class="opacity-75 mb-2">Gestiona las especialidades que pueden tener los médicos</span>
             </div>
-            <?php if (tienePermiso('', Permiso::ACTUALIZAR)): ?>
+            <?php if (tienePermiso('especialidades', Permiso::ACTUALIZAR)): ?>
                 <div>
                     <button style="padding: .65rem 1.4rem;"
                         class="btn btn-outline-light rounded-pill"
